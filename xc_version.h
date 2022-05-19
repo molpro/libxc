8823b60425f5300208f411d0ba7ca6cd184c1fb0
Andy May <ajmay81@gmail.com>
2022-05-19 15:17:35 +0200

    libxc-5.2.3.tar.gz

--- xc_version.h
+++ xc_version.h
@@ -15,10 +15,10 @@
 extern "C" {
 #endif
 
-#define XC_VERSION "5.2.2"
+#define XC_VERSION "5.2.3"
 #define XC_MAJOR_VERSION 5
 #define XC_MINOR_VERSION 2
-#define XC_MICRO_VERSION 2
+#define XC_MICRO_VERSION 3
 
 #ifdef __cplusplus
 }
